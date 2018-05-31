7991c9eb32c62e48306f12737d46f07d93a76537
Paul Boone <narcissus.pong@gmail.com>
2018-05-31 17:39:57 -0400

    use standard ranges

--- npy_block_avg.py
+++ npy_block_avg.py
@@ -14,18 +14,23 @@ num_rows, num_molecules, num_cols = data.shape
 d1 = data[:,1,:]
 d2 = data[:,2,:]
 
+max_row = 1024
 #create dataframe with 1024 rows
-df = pd.DataFrame(d1[0:1024])
+df = pd.DataFrame(d1[0:max_row])
 
 mean = df.mean()
-variances = np.array([df.groupby(lambda x: x // (2**i)).mean().var(ddof=0) for i in range(0,9)])
-total_points = np.array([1024 / 2**i for i in range(0,9)])
+
+block_transformations = np.arange(0,9)
+block_sizes = 2**block_transformations
+variances = np.array([df.groupby(lambda x: x // i).mean().var(ddof=0) for i in block_sizes])
+
+total_points = max_row / block_sizes
 adj_variances = variances / ((total_points-1)[:,None])
 
-errors = np.sqrt(2/points_per_group) * adj_variances[:,1]
+errors = np.sqrt(2/total_points) * adj_variances[:,1]
 
 fig = plt.figure(figsize=(7,7))
 ax = fig.add_subplot(1, 1, 1)
-ax.errorbar(range(0,9),adj_variances[:,1], errors, zorder=2)
+ax.errorbar(block_transformations,adj_variances[:,1], errors, zorder=2)
 ax.set_xlabel('# block transformations applied')
 ax.set_ylabel('variance / (N-1)')
