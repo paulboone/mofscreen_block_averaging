dc4ed264fecd9c3093776bca1baee570de58936b
Paul Boone <narcissus.pong@gmail.com>
2018-06-13 09:56:28 -0400

    calculate across multiple molecules; output as txt / numpy array; normalize

--- msd_calc_x_one_molecule.py
+++ msd_calc_x_one_molecule.py
@@ -19,33 +19,37 @@ num_rows, num_molecules, num_cols = data.shape
 d0 = data[:,0,0] # x for first molecule
 
 # max_row = 4096
-max_row = 4096000
+max_row = 50000
 corr_time = 2048
+num_molecules = 10
 
 start_time = time.time()
 prev_time = start_time
 
-
-sumdx = np.zeros(max_row)
-originx = np.zeros(max_row // corr_time)
-for row in range(max_row):
-    rel_index = row % corr_time
-
-    for i in range(row // corr_time + 1):
-        origin_row = i * corr_time
-        drow = row - origin_row
-        dx = d0[row] - originx[i]
-        sumdx[drow] += dx**2
+norm = np.zeros(max_row)
+sumd = np.zeros((max_row, 3))
+origin = np.zeros((math.ceil(max_row / corr_time), num_molecules, 3))
+for row_index in range(max_row):
+    rel_index = row_index % corr_time
 
     if rel_index == 0:
-        originx[row // corr_time] = d0[row]
+        origin[row_index // corr_time] = data[row_index]
 
+    for origin_index in range(row_index // corr_time + 1):
+        drow = row_index - origin_index * corr_time
+        sumd[drow] += (data[row_index] - origin[origin_index]).sum(axis=0)**2
+        norm[drow] += num_molecules
 
-    if row % 10000 == 0:
+    # print out progress
+    if row_index % 10000 == 0:
         t = time.time()
-        print("%.1fs (delta %5.1fs): %i" % (t - start_time, t - prev_time, row))
+        print("%.1fs (delta %5.1fs): %i" % (t - start_time, t - prev_time, row_index))
         prev_time = t
 
+sumd /= norm[:,None]
+
+np.save('tau_vs_t.npy', sumd)
+np.savetxt('tau_vs_t.txt', sumd)
 
 #
 # fig = plt.figure(figsize=(7,7))
