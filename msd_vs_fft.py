a09b319f1338b45976156eeb51cf701b5541a441
Paul Boone <narcissus.pong@gmail.com>
2018-06-28 13:17:29 -0400

    add linear fits to msd calc

--- msd_vs_fft.py
+++ msd_vs_fft.py
@@ -1,3 +1,13 @@
+#Compare via running:
+# %timeit results = msd_straight_forward(r)
+# %timeit results_fft = msd_fft(r)
+
+# results = msd_straight_forward(r)
+# r.shape
+
+# r = np.cumsum(np.random.choice([-1., 0., 1.], size=(N, 3)), axis=0)
+# results = msd_fft(r)
+
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -44,12 +54,14 @@ def msd_fft(r):
 N = 4000000
 reduce_points = 4000
 filename = "lammpstrj.npy"
+fs_per_row = 10
+
 t = np.arange(0,N)
-simple_t = np.mean(t.reshape(-1,reduce_points), axis=1)
-# row:molecule:x,y,z
-data = np.load(filename)
+simple_t = np.mean(t.reshape(-1,reduce_points), axis=1) * fs_per_row / 1e6
+data = np.load(filename) # row:molecule:x,y,z
 num_rows, num_molecules, num_cols = data.shape
 
+# per molecule plots
 fig = plt.figure(figsize=(7,7*num_molecules))
 all_results = np.zeros(int(N / reduce_points))
 for m in range(num_molecules):
@@ -64,32 +76,44 @@ for m in range(num_molecules):
     ax.plot(simple_t, simple_results, zorder=2)
 
 fig.savefig("msd_fft_molecule_plots.png", dpi=288)
-
-
 all_results /= num_molecules
+
+
+# attempted fits across different ranges
+lin_fit_pairs = [(0.0,1.0), (0.10,0.50), (0.10,0.45), (0.10,0.40), (0.10,0.35), (0.10,0.30)]
+results = []
+lowest_error = None
+lowest_error_pair = None
+for pair in lin_fit_pairs:
+    # y = at + b
+    len(simple_t)
+    p1 = int(pair[0]* N / reduce_points)
+    p2 = int(pair[1]* N / reduce_points)
+    poly, residuals, rank, _, _ = np.polyfit(simple_t[p1:p2], all_results[p1:p2],1, full=True,)
+    error = residuals / (p2 - p1)
+
+    # pick best fit
+    if not lowest_error or error < lowest_error:
+        lowest_error = error
+        lowest_error_pair = (p1,p2)
+    results.append([(p1,p2), error, poly])
+
+# plot combined data and fits
 fig = plt.figure(figsize=(7,7))
 ax = fig.add_subplot(1, 1, 1)
-ax.set_xlabel('tau [fs?]')
-ax.set_ylabel('MSD')
+ax.set_xlabel('tau [ps]')
+ax.set_ylabel('MSD [Ang^2]')
 ax.grid(linestyle='-', color='0.7', zorder=0)
-ax.plot(simple_t, all_results, zorder=2)
+ax.plot(simple_t, all_results, zorder=10)
+
+for r in results:
+    p, error, poly = r
+    zorder = 2
+    if p == lowest_error_pair:
+        zorder = 20
+
+    ax.plot(simple_t[p[0]:p[1]], np.polyval(poly, simple_t[p[0]:p[1]]), zorder=zorder,
+            label="(%.2f-%.2f; %.2E) %2.0ft + %2.0f" % (*p, error, *poly))
+
+ax.legend()
 fig.savefig("msd_fft_all_plot.png", dpi=288)
-
-
-#Compare via running:
-
-# %timeit results = msd_straight_forward(r)
-# %timeit results_fft = msd_fft(r)
-
-
-# results = msd_straight_forward(r)
-# r.shape
-
-# r = np.cumsum(np.random.choice([-1., 0., 1.], size=(N, 3)), axis=0)
-# results = msd_fft(r)
-
-
-# ax.set_xlim(0,t_end)
-# ax.legend(['C_a'])
-# ax.set_title('Concentration vs Time' + subheader)
-# fig.savefig("conc-vs-time.png", dpi=288)
